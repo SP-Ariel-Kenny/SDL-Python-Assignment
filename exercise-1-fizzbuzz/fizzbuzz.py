b7441f04dea9851ca4e18a05f046eeb0f6d89d15
SP-Ariel-Kenny <ariel.kenny@ucdconnect.ie>
2026-09-21 18:01:00 +0100

    Implement fizzbuzz function to print numbers and Fizz/Buzz

--- exercise-1-fizzbuzz/fizzbuzz.py
+++ exercise-1-fizzbuzz/fizzbuzz.py
@@ -1,1 +1,14 @@
+# define fizzbuzz game
+def fizzbuzz(range):
+  for num in range:
+    if num % 3 == 0:
+      if num % 5 == 0:
+        print("FizzBuzz")
+      else:
+        print("Fizz")
+    elif num % 5 == 0:
+      print("Buzz")
+    else:
+      print(f"{num}")
 
+fizzbuzz(100)
