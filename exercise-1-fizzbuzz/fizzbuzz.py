2dc4c81e1bd46cd7ec7a77a2dfb89e8562686315
SP-Ariel-Kenny <ariel.kenny@ucdconnect.ie>
2026-09-21 18:19:28 +0100

    Update fizzbuzz.py

--- exercise-1-fizzbuzz/fizzbuzz.py
+++ exercise-1-fizzbuzz/fizzbuzz.py
@@ -1,28 +1,26 @@
 # define fizzbuzz game
 def fizzbuzz(i):
   for num in range(1,i+1):
+    F = ""
+    B = ""
+    f = ""
+    b = ""
+    flip = False
     if num % 3 == 0:
-      if num % 5 == 0:
-        if (num % 7 == 0) and (num % 11 == 0):
-          print("FizzBuzzFangBang")
-        elif num % 7 == 0:
-          print("FizzBuzzFang")
-        elif num % 11 == 0:
-          print("FizzBuzzFang")
-        else:
-          print("FuzzBuzz")
-      else:
-        print("Fizz")
-    elif num % 5 == 0:
-      if (num % 7 == 0) and (num % 11 == 0):
-        print("BuzzFangBang")
-      elif num % 7 == 0:
-        print("BuzzFang")
-      elif num % 11 == 0:
-        print ("BuzzFangBang")
-      else:
-        print("Buzz")
-    else:
+      F = "Fizz"
+      flip = True
+    if num % 5 == 0:
+      B = "Buzz"
+      flip = True
+    if num % 7 == 0:
+      f = "Fang"
+      flip = True
+    if num % 11 == 0:
+      b = "Bang"
+      flip = True
+    if flip = False:
       print(f"{num}")
+    elif flip = True:
+      print(f"{F}{B}{f}{b}")
 
 fizzbuzz(100)
