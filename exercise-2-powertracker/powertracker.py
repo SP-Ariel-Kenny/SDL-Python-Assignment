3c4eefe9928c2cc59916ee6695831e76739248ec
SP-Ariel-Kenny <ariel.kenny@ucdconnect.ie>
2026-09-21 18:51:42 +0100

    Implement random power calculations and output results
    Add random number generation and power calculations in a loop.

--- exercise-2-powertracker/powertracker.py
+++ exercise-2-powertracker/powertracker.py
@@ -1,1 +1,24 @@
+import random
 
+flip = True
+nums = []
+index = 0
+
+while flip == True:
+  i = random.randint(1, 20)
+  power = random.randint(2, 3)
+  num = i ** power
+  nums.append(num)
+  print(f"Loop {index+1}: {i}^{power} = {num}")
+  if nums[index] % nums[index - 1] == 0:
+    break
+  else:
+    index += 1
+
+nums_sorted = nums.sort()
+
+# output data
+print(f"The largest result is {nums_sorted[-1]}")
+print(f"The smallest result is {nums_sorted[0]}")
+print(f"{nums[-1]} is divisible by {nums[-2]}.")
+print(f"We completed {index+1} loops.")
