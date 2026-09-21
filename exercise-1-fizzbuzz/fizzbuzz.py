69c8f0604458a509a09c42b78b20809e8ce10534
SP-Ariel-Kenny <ariel.kenny@ucdconnect.ie>
2026-09-21 18:13:05 +0100

    Update fizzbuzz.py

--- exercise-1-fizzbuzz/fizzbuzz.py
+++ exercise-1-fizzbuzz/fizzbuzz.py
@@ -3,11 +3,25 @@ def fizzbuzz(i):
   for num in range(1,i+1):
     if num % 3 == 0:
       if num % 5 == 0:
-        print("FizzBuzz")
+        if (num % 7 == 0) and (num % 11 == 0):
+          print("FizzBuzzFangBang")
+        elif num % 7 == 0:
+          print("FizzBuzzFang")
+        elif num % 11 == 0:
+          print("FizzBuzzFang")
+        else:
+          print("FuzzBuzz")
       else:
         print("Fizz")
     elif num % 5 == 0:
-      print("Buzz")
+      if (num % 7 == 0) and (num % 11 == 0):
+        print("BuzzFangBang")
+      elif num % 7 == 0:
+        print("BuzzFang")
+      elif num % 11 == 0:
+        print ("BuzzFangBang")
+      else:
+        print("Buzz")
     else:
       print(f"{num}")
 
