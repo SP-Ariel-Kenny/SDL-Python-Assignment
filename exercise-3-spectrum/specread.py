9b94a835cb0dbe8ba8bc54f1a88a859b977d3fe7
SP-Ariel-Kenny <ariel.kenny@ucdconnect.ie>
2026-09-22 10:25:07 +0100

    Change file reading method to use CSV reader

--- exercise-3-spectrum/specread.py
+++ exercise-3-spectrum/specread.py
@@ -2,6 +2,6 @@ import csv
 import scipy as sp
 
 with open('spectrum.txt', 'r') as file:
-  for line in file.readlines():
-    line = line.strip()
-    print(line)
+  reader = csv.reader(file, delimiter=',')
+  for row in reader:
+    print(row)
