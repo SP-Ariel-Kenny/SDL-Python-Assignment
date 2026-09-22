04a5fdddcab7438e21bc658d3d49aaabf4dc3e19
SP-Ariel-Kenny <ariel.kenny@ucdconnect.ie>
2026-09-22 10:37:27 +0100

    Update specread.py

--- exercise-3-spectrum/specread.py
+++ exercise-3-spectrum/specread.py
@@ -1,9 +1,7 @@
 import csv
 import scipy as sp
 
-spec_list = []
 with open('spectrum.txt', 'r') as file:
   reader = csv.reader(file, delimiter=',')
   for row in reader:
-    spec_list.append(list(map(float, row)))
-spec_list
+    pass
