ba0d698633b19853604c9b0aeb5a3f01138acca7
SP-Ariel-Kenny <ariel.kenny@ucdconnect.ie>
2026-09-22 10:34:52 +0100

    Refactor spectrum data reading to use map function

--- exercise-3-spectrum/specread.py
+++ exercise-3-spectrum/specread.py
@@ -5,6 +5,5 @@ spec_list = []
 with open('spectrum.txt', 'r') as file:
   reader = csv.reader(file, delimiter=',')
   for row in reader:
-    wavelength, flux = row.split(',')
-    spec_list.append([float(wavelength), float(flux)])
+    spec_list.append(list(map(float, row)))
 spec_list
